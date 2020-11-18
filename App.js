e1f74e1199a11e2e581c67217951d8c4644768e7
patryks9090@gmail.com <Patryk0.1>
2020-11-18 12:55:06 +0100

    eval

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {Component,useEffect} from 'react';
-import SplashScreen from 'react-native-splash-screen'
+import React, {Component, useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import {
     StyleSheet,
@@ -12,6 +12,7 @@ import {
 import styles from './styles';
 import OwnTouchableOpacity from './OwnTouchableOpacity';
 
+var mexp = require('math-expression-evaluator');
 const portraitButtons = [
     {content: 'AC', color: 'gray', width: '25%'}, {content: '+/-', color: 'gray', width: '49.6%'}, {
         content: '+',
@@ -111,6 +112,7 @@ export default class App extends Component {
             screenWidth: null,
             screenHeight: null,
             landscape: isLandscape(),
+            available: false,
         };
 
         Dimensions.addEventListener('change', () => {
@@ -122,155 +124,160 @@ export default class App extends Component {
 
 
     buttonPressed(text) {
-        if (text == '1' || text == '2' || text == '3' || text == '4' || text == '5' || text == '6' || text == '7' || text == '8' || text == '9' || text == '0') {
-            this.setState({
-                calculations: (this.state.calculations == 0 && this.state.isDot == false) ? text : this.state.calculations + text.toString(),
-            });
-        } else if (text == '.') {
-            if (this.state.isDot == false) {
-                this.setState({
-                    isDot: true,
-                    calculations: ((this.state.calculations == '') ? '0.' : this.state.calculations + '.'),
-                });
-            }
-        } else if (text == 'AC') {
+        if (text == 'AC') {
             this.setState({
                 resultText: '',
                 calculations: '0',
                 numSign: '+',
                 operationSign: '',
                 isDot: false,
+                available: false,
             });
-        } else if (text == '+/-') {
-            let tempString = this.state.calculations.toString();
-            let minTempString = tempString.substring(1, tempString.length);
-            if (this.state.calculations != 0) {
+        } else if (this.state.calculations != 'Infinity') {
+            if (text == '1' || text == '2' || text == '3' || text == '4' || text == '5' || text == '6' || text == '7' || text == '8' || text == '9' || text == '0') {
                 this.setState({
-                    calculations: (this.state.numSign == '+') ? '-' + tempString : minTempString,
-                    numSign: (this.state.numSign == '+') ? '-' : '+',
+                    calculations: (this.state.calculations == 0 && this.state.isDot == false) ? text : this.state.calculations + text.toString(),
+                    available: true,
                 });
-            }
-        } else if (text == '=') {
-            if (this.state.resultText != '') {
-                let firstInput = this.state.resultText.substring(0, this.state.resultText.length - 1);
-                let mathOperation = this.state.resultText.substring(this.state.resultText.length - 1, this.state.resultText.length);
-                let secondInput = this.state.calculations;
-                if (mathOperation == '/' && secondInput == 0.0) {
-                    this.showToast('cant divide by 0');
-                } else {
-                    let result = makeCount[mathOperation](parseFloat(firstInput), parseFloat(secondInput));
+            } else if (text == '.') {
+                if (this.state.isDot == false) {
+                    this.setState({
+                        isDot: true,
+                        calculations: ((this.state.calculations == '') ? '0.' : this.state.calculations + '.'),
+                    });
+                }
+            } else if (text == '+/-') {
+                let tempString = this.state.calculations.toString();
+                let minTempString = tempString.substring(1, tempString.length);
+                if (this.state.calculations != 0) {
+                    this.setState({
+                        calculations: (this.state.numSign == '+') ? '-' + tempString : minTempString,
+                        numSign: (this.state.numSign == '+') ? '-' : '+',
+                    });
+                }
+            } else if (text == '=') {
+                if (this.state.resultText != '') {
+
+                    let result = mexp.eval(this.state.resultText + this.state.calculations);
                     this.setState({
                         operationSign: '',
                         calculations: result,
                         resultText: '',
                         numSign: result < 0 ? '-' : '+',
+                        available: true,
+                    });
+
+                }
+            } else if (text == '%') {
+                if (this.state.calculations != 0) {
+                    this.setState({
+                        calculations: this.state.calculations / 100,
+                        isDot: this.state.calculations < 99 ? true : false,
                     });
                 }
-            }
-        } else if (text == '%') {
-            if (this.state.calculations != 0) {
+            } else if (text == 'pi') {
                 this.setState({
-                    calculations: this.state.calculations / 100,
-                    isDot: this.state.calculations < 99 ? true : false,
+                    calculations: 'pi',
+                    available: true,
                 });
-            }
-        } else if (text == '√x') {
-            if (this.state.calculations != 0) {
+            } else if (text == 'e') {
                 this.setState({
-                    isDot: (Math.sqrt(this.state.calculations).toString().includes('.') == false) ? false : true,
-                    calculations: Math.sqrt(this.state.calculations),
+                    calculations: 'e',
+                    available: true,
                 });
-            }
-        } else if (text == 'x!') {
-            if (this.state.calculations != 0 && (this.state.calculations).toString().includes('.') == false) {
-                let result = 1;
-                let count = this.state.calculations;
-                while (count > 1) {
-                    result = result * count;
-                    count--;
-                }
-                this.setState({
-                    calculations: result,
-                });
-            } else {
-                this.showToast('musi byc liczba calkowita');
-            }
-        } else if (text == 'pi') {
-            this.setState({
-                calculations: Math.PI.toFixed(5),
-            });
-        } else if (text == 'e') {
-            this.setState({
-                calculations: Math.E.toFixed(5),
-            });
-        } else if (text == 'e^x') {
-            this.setState({
-                calculations: Math.pow(Math.E, Math.round(this.state.calculations)).toFixed(4),
-                isDot: true,
-            });
-
-        } else if (text == '10^x') {
-            this.setState({
-                calculations: Math.pow(10, Math.round(this.state.calculations)),
-                isDot: false,
-            });
-
-        } else if (text == 'ln(x)') {
-            if (this.state.calculations > 0) {
-                this.setState({
-                    calculations: Math.log(this.state.calculations).toFixed(5),
-                    isDot: true,
-                });
-            } else {
-                this.showToast('liczba musi byc wieksza od 0');
-            }
-
-        } else if (text == 'log10') {
-            if (this.state.calculations > 0) {
-                this.setState({
-                    calculations: Math.log10(this.state.calculations).toFixed(5),
-                    isDot: true,
-                });
-            } else {
-                this.showToast('liczba musi byc wieksza od 0');
-            }
-
-        } else if (text == 'x^2') {
-            this.setState({
-                calculations: Math.pow(this.state.calculations, 2),
-            });
-        } else if (text == 'x^3') {
-            this.setState({
-                calculations: Math.pow(this.state.calculations, 3),
-            });
-        } else {
-            if (this.state.operationSign == '') {
-                this.setState({
-                    operationSign: text,
-                    resultText: this.state.calculations + text,
-                    calculations: '0',
-                    numSign: '+',
-                });
-            } else {
-                let firstInput = parseFloat(this.state.resultText.substring(0, this.state.resultText.length - 1));
-                let mathOperation = this.state.resultText.substring(this.state.resultText.length - 1, this.state.resultText.length);
-                let secondInput = parseFloat(this.state.calculations);
-                if (mathOperation == '/' && secondInput == 0.0) {
-                    this.showToast('cant divide by 0');
-                } else {
+            } else if (text == '+' || text == '-' || text == '/' || text == '*') {
+                if (this.state.operationSign == '') {
                     this.setState({
                         operationSign: text,
-                        resultText: makeCount[mathOperation](firstInput, secondInput) + text,
+                        resultText: this.state.calculations + text,
                         calculations: '0',
                         numSign: '+',
+                        available: true,
+                    });
+                } else {
+                    if (text == '/' && this.state.calculations == 0.0) {
+                        this.showToast('cant divide by 0');
+                    } else {
+                        this.setState({
+                            operationSign: text,
+                            resultText: this.state.resultText + this.state.calculations + text,
+                            calculations: '0',
+                            numSign: '+',
+                            available: true,
+                        });
+                    }
+                }
+            } else if (this.state.available == true || this.state.calculations != 0.0) {
+                if (this.state.calculations != '') {
+                    if (text == 'x^2') {
+                        this.setState({
+                            calculations: mexp.eval(this.state.calculations + '^2'),
+                        });
+                    } else if (text == 'x^3') {
+                        this.setState({
+                            calculations: mexp.eval(this.state.calculations + '^3'),
+                        });
+                    } else if (text == 'e^x') {
+                        this.setState({
+                            calculations: mexp.eval('e^' + this.state.calculations).toFixed(5),
+                            isDot: true,
+                        });
+
+                    } else if (text == '10^x') {
+                        this.setState({
+                            calculations: mexp.eval('10^' + this.state.calculations),
+                            isDot: false,
+                        });
+
+                    } else if (text == 'ln(x)') {
+                        if (this.state.calculations > 0) {
+                            this.setState({
+                                calculations: mexp.eval('log' + this.state.calculations).toFixed(2),
+                                isDot: true,
+                            });
+                        } else {
+                            this.showToast('liczba musi byc wieksza od 0');
+                        }
+
+                    } else if (text == 'log10') {
+                        if (this.state.calculations > 0) {
+                            this.setState({
+                                calculations: mexp.eval('log' + this.state.calculations).toFixed(2),
+                                isDot: true,
+                            });
+                        } else {
+                            this.showToast('liczba musi byc wieksza od 0');
+                        }
+                    } else if (text == '√x') {
+                        if (this.state.calculations != 0) {
+                            this.setState({
+                                isDot: (mexp.eval('root' + this.state.calculations).toString().includes('.') == false) ? false : true,
+                                calculations: mexp.eval('root' + this.state.calculations).toFixed(2),
+                            });
+                        }
+                    } else if (text == 'x!') {
+                        if (this.state.calculations != 0 && (this.state.calculations).toString().includes('.') == false) {
+                            this.setState({
+                                calculations: mexp.eval(this.state.calculations + '!'),
+                            });
+                        } else {
+                            this.showToast('musi byc liczba calkowita');
+                        }
+                    }
+                    this.setState({
+                        available: false,
                     });
                 }
+            } else {
+                this.showToast('podaj liczbe');
             }
+        } else {
+            this.showToast("zacznij od nowa")
         }
     }
 
     showToast(txt) {
-        ToastAndroid.showWithGravity(txt, ToastAndroid.LONG, ToastAndroid.TOP);
+        ToastAndroid.showWithGravity(txt, ToastAndroid.SHORT, ToastAndroid.TOP);
     }
 
     render() {
@@ -288,14 +295,16 @@ export default class App extends Component {
                         this.state.landscape == false ?
                             portraitButtons.map((el) => {
                                 return (
-                                    <OwnTouchableOpacity key={el.content} content={el.content} color={el.color} width={el.width}
+                                    <OwnTouchableOpacity key={el.content} content={el.content} color={el.color}
+                                                         width={el.width}
                                                          onBtnPress={this.buttonPressed.bind(this, el.content)}/>
                                 );
                             })
                             :
                             landscapeButtons.map((el) => {
                                 return (
-                                    <OwnTouchableOpacity key={el.content} content={el.content} color={el.color} width={el.width}
+                                    <OwnTouchableOpacity key={el.content} content={el.content} color={el.color}
+                                                         width={el.width}
                                                          onBtnPress={this.buttonPressed.bind(this, el.content)}/>
                                 );
                             })
@@ -306,17 +315,3 @@ export default class App extends Component {
     }
 }
 
-const makeCount = {
-    '+': function (x, y) {
-        return x + y;
-    },
-    '-': function (x, y) {
-        return x - y;
-    },
-    '*': function (x, y) {
-        return x * y;
-    },
-    '/': function (x, y) {
-        return x / y;
-    },
-};
